78e28249906d6e80d07877e8ad3873938681edeb
Олег Трушков <oleg_trushkov@hotmail.com>
2021-10-04 04:25:13 +0300

    cleaned

--- TransformationEstimation/TransformationEstimation.cpp
+++ TransformationEstimation/TransformationEstimation.cpp
@@ -3,43 +3,49 @@
 
 #include "TransformationEstimation.h"
 
+using namespace std;
+using namespace pcl;
+using namespace pcl::registration;
+using namespace Eigen;
+
 int main()
 {
-	pcl::PointCloud<pcl::PointXYZ> src, tgt;
-	auto p1 = pcl::PointXYZ(0, 0, 0);
-	auto p2 = pcl::PointXYZ(20.000, 80.042, 2.530);
-	auto p3 = pcl::PointXYZ(60.000, 10.042, -5.530);
-	src.push_back(p1);
-	src.push_back(p2);
-	src.push_back(p3);
+	PointCloud<PointXYZ>::Ptr src(new PointCloud<PointXYZ>);
+	auto p1 = PointXYZ(0, 0, 0);
+	auto p2 = PointXYZ(20.000, 80.042, 2.530);
+	auto p3 = PointXYZ(60.000, 10.042, -5.530);
+	src->push_back(p1);
+	src->push_back(p2);
+	src->push_back(p3);
 
-	auto p3t = pcl::PointXYZ(20, 10, -20);
-	auto p1t = pcl::PointXYZ(-22.456, 80.740, -17.470);
-	auto p2t = pcl::PointXYZ(55.326, 59.527, -25.530);
-	tgt.push_back(p1t);
-	tgt.push_back(p2t);
-	tgt.push_back(p3t);
+	PointCloud<PointXYZ>::Ptr tgt(new PointCloud<PointXYZ>);
+	auto p1t = PointXYZ(20, 10, -20);
+	auto p2t = PointXYZ(-22.456, 80.740, -17.470);
+	auto p3t = PointXYZ(55.326, 59.527, -25.530);
+	tgt->push_back(p1t);
+	tgt->push_back(p2t);
+	tgt->push_back(p3t);
 
-	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_in(new pcl::PointCloud<pcl::PointXYZ>(src));
-	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_out(new pcl::PointCloud<pcl::PointXYZ>(tgt));
-
-	//correspondence
-	pcl::registration::CorrespondenceEstimation<pcl::PointXYZ, pcl::PointXYZ> est;
-	est.setInputSource(cloud_in);
-	est.setInputTarget(cloud_out);
-	pcl::Correspondences all_correspondences;
-	est.determineReciprocalCorrespondences(all_correspondences);
+	//correspondences
+	CorrespondenceEstimation<PointXYZ, PointXYZ> ce;
+	ce.setInputSource(src);
+	ce.setInputTarget(tgt);
+	Correspondences all_correspondences;
+	ce.determineReciprocalCorrespondences(all_correspondences);
 
 	//svd
-	pcl::registration::TransformationEstimationSVD<pcl::PointXYZ, pcl::PointXYZ> te;
-	pcl::registration::TransformationEstimationSVD<pcl::PointXYZ, pcl::PointXYZ>::Matrix4 R;
-	te.estimateRigidTransformation(src, tgt, all_correspondences, R);
-	Eigen::Matrix3f b = R.topLeftCorner<3, 3>();
-	auto euler = b.eulerAngles(2, 1, 0);
-	euler *= 180 / M_PI;
-	std::cout << R << std::endl;
-	std::cout << euler << std::endl;
-
-	std::cin.get();
+	TransformationEstimationSVD<PointXYZ, PointXYZ> te;
+	Matrix4f t;
+	//te.estimateRigidTransformation(*src, *tgt, all_correspondences, t);
+	te.estimateRigidTransformation(*src, *tgt, t);
+	Matrix3f r = t.topLeftCorner<3, 3>();
+	Vector3f e = r.eulerAngles(2, 1, 0);
+	e *= 180 / M_PI;
+	cout << "Transformation matrix:" << endl;
+	cout << t << endl;
+	cout << endl;
+	cout << "Euler angles:" << endl;
+	cout << e << endl;
+	cin.get();
 	return 0;
 }
